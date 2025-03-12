43c0615585c0a4ed9779b31339746ba9aaf8b554
owlks4 <115742200+owlks4@users.noreply.github.com>
2025-03-12 16:29:04 +0000

    Move randomiser button to two individual dice icons

--- docs/index.js
+++ docs/index.js
@@ -159,10 +159,12 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-document.getElementById("random").onclick = () => {  
+document.getElementById("randomise-A").onclick = () => {  
   charaA.value = decodeName(charmap.characters[getRandomInt(charmap.characters.length)].name);
+};
+
+document.getElementById("randomise-B").onclick = () => {  
   charaB.value = decodeName(charmap.characters[getRandomInt(charmap.characters.length)].name);
-  goButton.click();
 };
 
 document.getElementById("dotd-disclaimer").onclick = ()=>{
@@ -170,7 +172,7 @@ document.getElementById("dotd-disclaimer").onclick = ()=>{
 }
 
 document.getElementById("longest-connection-info").onclick = ()=>{
-  alert("When you exclude the Doctor, I think the 'worst' connections are from:\n\n• any character exclusive to Class who was NOT in 'For Tonight we Might Die' (e.g. Dorothea Ames)\n\nTO\n\n• any character who only appeared in 'The Deadly Assassin' (e.g. Goth)\n\nAnother good shout is Mission to the Unknown, but it tends to have a slightly lower score due to some easy New Who connections to the 60s, like Ian Chesterton.\n\nWhen not using the Doctor, Mission to the Unknown is probably your best bet. As of writing this, you can even get 5th degree, Doctor-inclusive connections between Mission to the Unknown and Joy to the World, though I imagine that will change as the 15th Doctor becomes better-connected.")
+  alert("When you exclude the Doctor, I think the longest connection between any two characters is 8, from:\n\n• any character exclusive to Class who was NOT in 'For Tonight we Might Die' (e.g. Dorothea Ames)\n\nTO\n\n• any character who only appeared in 'The Deadly Assassin' (e.g. Spandrell)\n\nAnother good shout is Mission to the Unknown, but it tends to have a slightly lower score due to some easy New Who connections to the 60s, like Ian Chesterton.\n\nWhen not using the Doctor, Mission to the Unknown is probably your best bet. As of writing this, you can even get 5th degree, Doctor-inclusive connections between Mission to the Unknown and Joy to the World, though I imagine that will change as the Fifteenth Doctor becomes better-connected.")
 }
 
 async function start(){
@@ -237,6 +239,11 @@ function attemptToFindConnection_BFS(start,end){
 
     let characters = charmap.characters;  
     let episodes = charmap.episodes;
+
+    if (start == -1 || end == -1){
+      console.log("Invalid start or end character");
+      return {"start":start,"end":end,"score":-1,"path":null};
+    }
 
     blacklist = character_names_blacklist.map((x) => {return get_char_ID_by_name(x)});
 
